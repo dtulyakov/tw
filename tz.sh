28f43f9ef1f943b0a9018cea8d5f91511e8ada4f
Denis Tulyakov <dtulyakov@gmail.com>
2015-12-29 23:30:02 +0300

    Добавил тесты curl  На ветке master  Ваша ветка обновлена в соответствии с «origin/master».
     Изменения, которые будут включены в коммит:
    	изменено:      tz.sh

--- tz.sh
+++ tz.sh
@@ -3,10 +3,11 @@
 LANG=C
 LC_ALL=C
 GIT=$(which git)
+ECHO=$(which echo)
 USER=$(whoami)
 ${GIT} clone https://github.com/dtulyakov/tw.git /home/${USER}/DOCKER
 GIT_DIR="/home/${USER}/DOCKER/"
-GIT_PROJ=$(ls ${GIT_DIR}|grep -v 'tz.sh')
+GIT_PROJ=$(ls ${GIT_DIR}|grep -v 'tz.sh\|README.md')
 
 
 ######## SCRIPTS
@@ -21,5 +22,11 @@ docker run --name docker-alice -d -p 81:80 test-docker-alice
 docker run --name docker-bob -d -p 82:80 test-docker-bob
 docker run --name docker-tom  -d -p 83:80 test-docker-tom
 docker run --name docker-nginx -d -p 80:80 test-docker-nginx
+
+curl docker-tom && ${ECHO}  "docker-tom OK" || ${ECHO} "docker-tom Err"
+${ECHO} ""
+curl docker-alice && ${ECHO} "docker-tom OK" || ${ECHO} "docker-tom Err"
+${ECHO} ""
+curl docker-bob && ${ECHO} "docker-tom OK" || ${ECHO} "docker-tom Err"
 
 #EOF
